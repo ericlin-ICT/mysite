7b88e1a6024bedf6d8aa40f37b0b4c1c4d62e6f9
ericlin-ICT <linxianghui1987@gmail.com>
2014-02-21 15:13:37 +0800

    ADAdmin V1.0

--- src/mysite/tables/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-from mysite.tables.models import Person
-
-def people(request):
-  return render(request, "people.html", {"people": Person.objects.all()})
--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -16,19 +16,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     #url(r'^admin/', include(admin.site.urls)),
 		url('^hello/$', hello),
-		#url('^time/$', current_datetime),
-		#url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-		#url('^meta_info/$', display_request_info),
-		#url('^meta_info_tpl/$', display_request_info_tpl),
-		#url('^search_form/$', search_form),
-		#url('^search/$', search),
-		#url('^contact/$', contact),
     url('^upload/$', upload_file),
     url('^process/$', process),
-    #url('^people/$', people),
 )
